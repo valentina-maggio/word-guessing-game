4d8c8bccad1bbb97871e03c51c2d787481c6f67c
Valentina <maggio.vale@gmail.com>
2022-06-08 10:47:39 +0200

    Extracting dictinary and word random method from Game class
    - create WordChoser class
    - create tests for the class using mockito to mock class and stub method

---
+++ app/src/main/java/game/WordChoser.java
@@ -0,0 +1,13 @@
+package game;
+
+import java.util.Random;
+
+public class WordChoser {
+  private static final String[] DICTIONARY = { "MAKERS", "CANDIES", "DEVELOPER", "LONDON" };
+
+  public String getRandomWordFromDictionary() {
+    Random rand = new Random();
+    int randomNumber = rand.nextInt(DICTIONARY.length);
+    return DICTIONARY[randomNumber];
+  }
+}
--- app/src/test/java/game/WordChoserTest.java
+++ app/src/test/java/game/WordChoserTest.java
@@ -1,5 +1,61 @@
 package game;
 
-public class WordChoserTest {
+import static org.mockito.Mockito.*;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class WordChoserTest {
+  @Test
+  public void testGetsRandomMakersWordToGuess() {
+    WordChoser mockedWordChoser = mock(WordChoser.class);
+    mockedWordChoser.getRandomWordFromDictionary();
+
+    verify(mockedWordChoser).getRandomWordFromDictionary();
+
+    when(mockedWordChoser.getRandomWordFromDictionary()).thenReturn("MAKERS");
+
+    System.out.println(mockedWordChoser.getRandomWordFromDictionary());
+    assertEquals(mockedWordChoser.getRandomWordFromDictionary(), "MAKERS");
+  }
+
+  @Test
+  public void testGetsRandomCandiesWordToGuess() {
+    WordChoser mockedWordChoser = mock(WordChoser.class);
+    mockedWordChoser.getRandomWordFromDictionary();
+
+    verify(mockedWordChoser).getRandomWordFromDictionary();
+
+    when(mockedWordChoser.getRandomWordFromDictionary()).thenReturn("CANDIES");
+
+    System.out.println(mockedWordChoser.getRandomWordFromDictionary());
+    assertEquals(mockedWordChoser.getRandomWordFromDictionary(), "CANDIES");
+  }
+
+  @Test
+  public void testGetsRandomDeveloperWordToGuess() {
+    WordChoser mockedWordChoser = mock(WordChoser.class);
+    mockedWordChoser.getRandomWordFromDictionary();
+
+    verify(mockedWordChoser).getRandomWordFromDictionary();
+
+    when(mockedWordChoser.getRandomWordFromDictionary()).thenReturn("DEVELOPER");
+
+    System.out.println(mockedWordChoser.getRandomWordFromDictionary());
+    assertEquals(mockedWordChoser.getRandomWordFromDictionary(), "DEVELOPER");
+  }
+
+  @Test
+  public void testGetsRandomLondonWordToGuess() {
+    WordChoser mockedWordChoser = mock(WordChoser.class);
+    mockedWordChoser.getRandomWordFromDictionary();
+
+    verify(mockedWordChoser).getRandomWordFromDictionary();
+
+    when(mockedWordChoser.getRandomWordFromDictionary()).thenReturn("LONDON");
+
+    System.out.println(mockedWordChoser.getRandomWordFromDictionary());
+    assertEquals(mockedWordChoser.getRandomWordFromDictionary(), "LONDON");
+  }
 }
